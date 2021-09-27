0d7f4251b1d8a2e286ca19538d9779d3a2ab24d1
ladudasha <darya.gameza@gmail.com>
2021-09-27 23:34:38 +0300

    diagram hover

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,3 +1,17 @@
+const circle__svg = document.querySelectorAll('.diagram__circle');
 
+console.log(circle__svg);
 
+const buttonVideo = document.querySelectorAll('.diagram__row-center');
+buttonVideo.forEach(el => {
+    el.addEventListener('mouseover', () => {
+        console.log(1);
+    })
+})
+
+circle__svg.forEach(el => {
+    el.addEventListener('mouseover', () => {
+        console.log(1);
+    })
+})
 //# sourceMappingURL=script.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,16 @@
+const circle__svg = document.querySelectorAll('.diagram__circle');
 
+console.log(circle__svg);
+
+const buttonVideo = document.querySelectorAll('.diagram__row-center');
+buttonVideo.forEach(el => {
+    el.addEventListener('mouseover', () => {
+        console.log(1);
+    })
+})
+
+circle__svg.forEach(el => {
+    el.addEventListener('mouseover', () => {
+        console.log(1);
+    })
+})
