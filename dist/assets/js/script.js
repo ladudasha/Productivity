32641354d8d08c46ff472180efdb97b47516a3b7
ladudasha <darya.gameza@gmail.com>
2022-02-13 13:50:52 +0300

    scroll#

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,2 +1,19 @@
+// плавный скролл
+
+
+  let anchors = document.querySelectorAll("a[href*='#']")
+  console.log(anchors);
+  for(let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+      event.preventDefault()
+      
+      let blockID  = anchor.getAttribute('href');
+      document.querySelector(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+
 
 //# sourceMappingURL=script.js.map
---
+++ src/assets/js/main.js
@@ -0,0 +1,17 @@
+// плавный скролл
+
+
+  let anchors = document.querySelectorAll("a[href*='#']")
+  console.log(anchors);
+  for(let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+      event.preventDefault()
+      
+      let blockID  = anchor.getAttribute('href');
+      document.querySelector(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+
